52998e78fdf0f0679846d456b07f46f125750113
lwisniewski96 <113308030+lwisniewski96@users.noreply.github.com>
2023-06-28 21:57:32 +0200

    hw1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+
+const photoPalette = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    img => `<div class="gallery__item">
+    <a class="gallery__link" href="${img.original}">
+      <img
+        class="gallery__image"
+        src="${img.preview}"
+            data-source="${img.original}"
+             alt="${img.description}"
+      />
+    </a>
+  </div>`
+  )
+  .join('');
+
+photoPalette.insertAdjacentHTML('afterbegin', markup);
 
 console.log(galleryItems);
+
+photoPalette.addEventListener('click', selectPhoto);
+
+function selectPhoto(event) {
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  event.preventDefault();
+
+  const instance = basicLightbox.create(
+    `
+    <img src="${event.target.dataset.source}" width="800" height="600">`
+  );
+
+  instance.show();
+}
